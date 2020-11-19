d1cb1792521a876d816a7ad43f7f217972750397
Zuzanna Szeloch <zuz.szeloch@gmail.com>
2020-11-19 19:57:23 +0100

    Add proper nesting to the code

--- js/script.js
+++ js/script.js
@@ -1,49 +1,60 @@
-{
-  function playGame(playerInput){
+{ 
 
-    function clearMessages(){
+  //function playGame(playerInput){
+  const playGame = function(playerInput){
+
+    //function clearMessages(){
+    const clearMessages = function(){
       document.getElementById('buttons').innerHTML = '';
     }
   
-    function getMoveName(argMoveId){
-      console.log('utworzona została funcja getMoveName z argumentem' +argMoveId);
-        if(argMoveId == 1){
-          return 'kamień';
-        } else if(argMoveId == 2){
-          return 'papier';
-        } else if(argMoveId == 3){
-          return 'nożyce';
-        } else {printMessage('Nie znam ruchu ' + argMoveId + '.');
-          return 'nieznany ruch';
-        }
+    //function getMoveName(argMoveId){
+    const getMoveName = function(argMoveId){
+    console.log('utworzona została funcja getMoveName z argumentem' +argMoveId);
+      if(argMoveId == 1){
+      return 'kamień';
+      } else if(argMoveId == 2){
+      return 'papier';
+      } else if(argMoveId == 3){
+      return 'nożyce';
+      } else {printMessage('Nie znam ruchu ' + argMoveId + '.');
+      return 'nieznany ruch';
       }
+    }
 
-      /*Computer*/
+    /*Computer and Player*/
 
     //let randomNumber = Math.floor(Math.random() * 3 + 1);
-    const randomNumber = Math.floor(Math.random() * 3 + 1);
+    const randomNumber = Math.floor(Math.random() * 3 + 1),
+          argComputerMove = getMoveName(randomNumber),
+          argPlayerMove = getMoveName(playerInput);
 
     console.log('Wylosowana liczba to: ' + randomNumber);
-
-    //let argComputerMove = getMoveName(randomNumber);
-    const argComputerMove = getMoveName(randomNumber);
+    console.log('Gracz wpisał: ' + playerInput);
 
     printMessage('Mój ruch to: ' + argComputerMove);
+    printMessage('Twój ruch to: ' + argPlayerMove);
+
+    //let argComputerMove = getMoveName(randomNumber);
+    //const argComputerMove = getMoveName(randomNumber);
+
+    //printMessage('Mój ruch to: ' + argComputerMove);
 
     /*Player*/
 
     //let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
-    console.log('Gracz wpisał: ' + playerInput);
+    //console.log('Gracz wpisał: ' + playerInput);
 
     //let argPlayerMove = getMoveName(playerInput);
-    const argPlayerMove = getMoveName(playerInput);
+    //const argPlayerMove = getMoveName(playerInput);
 
-    printMessage('Twój ruch to: ' + argPlayerMove);
+    //printMessage('Twój ruch to: ' + argPlayerMove);
 
     /*Results of the game*/
 
-    function displayResult(argComputerMove, argPlayerMove){
+    //function displayResult(argComputerMove, argPlayerMove){
+    const displayResult = function(argComputerMove, argPlayerMove){
     console.log('funkcja displayResult z argumentami: ' +argComputerMove + argPlayerMove);
     printMessage('Zagrałem ' + argComputerMove + ', a ty ' + argPlayerMove);
       if( argComputerMove == argPlayerMove){
@@ -80,6 +91,7 @@
   document.getElementById('play-scissors').addEventListener('click', function(){
     playGame('3');
   });
+
 }
 
 
